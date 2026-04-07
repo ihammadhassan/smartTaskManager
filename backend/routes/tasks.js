37b7cda5150b0cc6aff7841bfe168d96a09f54d8
Hammad Hassan <hammad.haassn@gmail.com>
2026-04-07 16:54:43 +0500

    Added API Versions

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -3,26 +3,26 @@ const Task = require("../models/Task");
 const auth = require("../middleware/auth");
 
 // Create Task
-router.post("/createTask", auth, async (req, res) => {
+router.post("/v1/createTask", auth, async (req, res) => {
   const task = new Task({ ...req.body, userId: req.user.userId });
   await task.save();
   res.status(201).json(task);
 });
 
 // Get Tasks
-router.get("/", auth, async (req, res) => {
+router.get("/v1/", auth, async (req, res) => {
   const tasks = await Task.find({ userId: req.user.userId });
   res.json(tasks);
 });
 
 //Update Task
-router.put("/updateTask/:id", auth, async(req, res) => {
+router.put("/v1/updateTask/:id", auth, async(req, res) => {
   const task = await Task.findByIdAndUpdate(req.params.id, {$set: req.body}, {returnDocument: "after"});
   res.json(task);
 });
 
 // Delete Task
-router.delete("/deleteTask/:id", auth, async (req, res) => {
+router.delete("/v1/deleteTask/:id", auth, async (req, res) => {
   await Task.findByIdAndDelete(req.params.id);
   res.json({ message: "Task deleted successfully" });
 });
