9220c9f30e040c67922bc55deb584cb31ac54a93
Hammad Hassan <hammad.haassn@gmail.com>
2026-04-07 18:07:59 +0500

    Added more styles

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -2,6 +2,7 @@ import { useState, useContext } from "react";
 import API from "../../utils/api";
 import { AuthContext } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import "../../styles/global.css";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -9,12 +10,24 @@ export default function Login() {
   const { login } = useContext(AuthContext);
   const [error, setError] = useState("");
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await API.post("/auth/login", { email, password });
-    login(res.data.token);
-    navigate("/dashboard");
+    setError("");
+
+    try {
+      setLoading(true);
+      const res = await API.post("/auth/login", { email, password });
+      login(res.data.token);
+      navigate("/dashboard");
+    } catch (err){
+      setError(err.response?.data?.error || "Signup failed");
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+    
   };
 
   return (
@@ -22,9 +35,21 @@ export default function Login() {
       <h2>Log In</h2>
       {error && <p style={{ color: "red" }}>{error}</p>}
       <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-        <input placeholder="Email" value={email} onChange={e=>setEmail(e.target.value)} />
-        <input type="password" placeholder="Password" value={password} onChange={e=>setPassword(e.target.value)} />
-        <button type="submit" style={{ padding: "10px", backgroundColor: "#4CAF50", color: "#fff", border: "none", borderRadius: "5px" }}>Login</button>
+        <input
+          type = "email"
+          placeholder="Email"
+          value={email}
+          onChange={e=>setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={e=>setPassword(e.target.value)}
+          required
+        />
+        <button type="submit" class = "primary-btn">Login</button>
       </form>
     <p style={{ marginTop: "10px" }}>
         Don't have an account? <span style={{ color: "blue", cursor: "pointer" }} onClick={() => navigate("/signup")}>Register</span>
